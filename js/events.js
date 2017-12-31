d77e352e962bc39f2c2612964ff65c9a8fee448c
mohsini172 <mohsini172@gmail.com>
2017-12-31 21:02:55 +0500

    first commit

---
+++ js/events.js
@@ -0,0 +1,37 @@
+document.body.onkeydown = function (e) {
+
+	if (e.keyCode == 38 && accelaration < 10) {
+		accelaration += 0.1;
+	}
+
+};
+
+document.body.onkeyup = function (e) {
+	if (e.keyCode == 38) {
+		reduceAccel(100);
+	}
+};
+
+function reduceAccel(index) {
+	if (index > 0 && accelaration > 0) {
+		setTimeout(function () {
+			accelaration -= 0.1;
+			reduceAccel(index--);
+		}, 10)
+	}
+}
+
+function onWindowResize() {
+
+	var aspect = window.innerWidth / window.innerHeight;
+
+	camera.left = - frustumSize * aspect / 2;
+	camera.right = frustumSize * aspect / 2;
+	camera.top = frustumSize / 2;
+	camera.bottom = - frustumSize / 2;
+
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
