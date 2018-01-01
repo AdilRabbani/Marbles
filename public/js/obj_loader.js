90100b1be9dbd71fcf7381b38f4376dc64dd4e57
mohsini172 <mohsini172@gmail.com>
2018-01-01 09:21:42 +0500

    changes in object code

--- public/js/obj_loader.js
+++ public/js/obj_loader.js
@@ -1,13 +1,12 @@
 function load_object(scene, url) {
 	var mtlLoader = new THREE.MTLLoader();
-	mtlLoader.setPath('../models/car1/');
-	mtlLoader.load('car_02.mtl', function (materials) {
+	mtlLoader.load('../models/car1/car_02.mtl', function (materials) {
 		materials.preload();
 		var objLoader = new THREE.OBJLoader2();
-		objLoader.setMaterials(materials);
-		objLoader.setPath('../models/car1/');
-		objLoader.load('car_02.obj', function (event) {
-			scene.add(event.detail.loaderRootNode);
+		objLoader.setMaterials(materials.materials);
+		objLoader.load('../models/car1/car_02.obj', function (event) {
+			var object = event.detail.loaderRootNode;
+			scene.add();
 		}, null, null, null, false);
 	});
 }
