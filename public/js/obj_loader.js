1459c078961008e63073625957c928076db9558c
mohsini172 <mohsini172@gmail.com>
2018-01-01 18:58:07 +0500

    Some changes in loading the obj files

--- public/js/obj_loader.js
+++ public/js/obj_loader.js
@@ -1,12 +1,12 @@
 function load_object(scene, url) {
 	var mtlLoader = new THREE.MTLLoader();
+	var objLoader = new THREE.OBJLoader2();
 	mtlLoader.load('../models/car1/car_02.mtl', function (materials) {
 		materials.preload();
-		var objLoader = new THREE.OBJLoader2();
 		objLoader.setMaterials(materials.materials);
 		objLoader.load('../models/car1/car_02.obj', function (event) {
 			var object = event.detail.loaderRootNode;
-			scene.add();
+			scene.add(object);
 		}, null, null, null, false);
 	});
 }
